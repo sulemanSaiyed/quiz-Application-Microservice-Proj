4b239de0d72b19b1b3c805a74d362f4bb3141fe4
Suleman Saiyed <sulemansaiyed20@gmail.com>
2025-07-16 13:17:24 +0530

    before genrating only req quiz like 5 4 now fetching quiz like etting question with option

--- src/main/java/com/denzil/quiz/controller/QuizController.java
+++ src/main/java/com/denzil/quiz/controller/QuizController.java
@@ -1,10 +1,14 @@
 package com.denzil.quiz.controller;
 
+import com.denzil.quiz.model.Question;
+import com.denzil.quiz.model.QuestionWrapper;
 import com.denzil.quiz.service.QuizService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
 
 @RestController
 @RequestMapping("Quiz")
@@ -16,5 +20,8 @@ public class QuizController {
 public ResponseEntity<String> createQuiz(@RequestParam String category, @RequestParam int numQ, @RequestParam String title) {
 
 return quizService.createQuiz(category,numQ, title);
+    }
+    public ResponseEntity<List<QuestionWrapper>> getQuizQuestions(@PathVariable Integer id) {
+return quizService.getQuizQuestion(id);
     }
 }
--- src/main/java/com/denzil/quiz/service/QuizService.java
+++ src/main/java/com/denzil/quiz/service/QuizService.java
@@ -3,6 +3,7 @@ package com.denzil.quiz.service;
 import com.denzil.quiz.dao.QuestionDao;
 import com.denzil.quiz.dao.QuizDao;
 import com.denzil.quiz.model.Question;
+import com.denzil.quiz.model.QuestionWrapper;
 import com.denzil.quiz.model.Quiz;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
@@ -11,6 +12,7 @@ import org.springframework.stereotype.Service;
 
 import java.util.ArrayList;
 import java.util.List;
+import java.util.Optional;
 
 @Service
 public class QuizService {
@@ -29,4 +31,15 @@ public class QuizService {
     }
 
 
+    public ResponseEntity<List<QuestionWrapper>> getQuizQuestion(Integer id) {
+       Optional<Quiz> quiz= quizDao.findById(id);
+       List<Question> questionFromDb= quiz.get().getQuestions();
+       List<QuestionWrapper> questionForUser= new ArrayList<>();
+       for (Question pp : questionFromDb) {
+           QuestionWrapper qw= new QuestionWrapper(pp.getId(), pp.getQuestionTitle(),pp.getOption1(), pp.getOption2(), pp.getOption3(),pp.getOption4());
+           questionForUser.add(qw);
+
+       }
+       return  new ResponseEntity<>(questionForUser,HttpStatus.OK);
+    }
 }
